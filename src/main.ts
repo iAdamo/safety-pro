4546e4d1728f6b409904f0c3c937e25f1f4c3896
Adam_Babatunde_Sanusi <iadamo.inc@gmail.com>
2024-10-29 00:15:09 +0100

    Implement API Versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Safety Pro')
+    .setDescription('The Safety Pro API description')
+    .setVersion('1.0')
+    .addTag('safety-pro')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
