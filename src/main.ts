cf597bda93400147589ba368daba5029694bb8b3
Adam_Babatunde_Sanusi <iadamo.inc@gmail.com>
2024-10-29 01:19:37 +0100

    Setting database register

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .addTag('safety-pro')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
