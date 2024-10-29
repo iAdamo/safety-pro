42c7f72d16e43a06e9e3640329cb4badb6b8f551
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-10-29 20:35:02 +0100

    Users Module - User Schema (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import databaseConfig from './config/database.config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       load: [databaseConfig],
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Set Global Prefix
+  app.setGlobalPrefix('api');
+
   // Enable Versioning
   app.enableVersioning({
     type: VersioningType.URI,
